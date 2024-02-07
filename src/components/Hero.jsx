9068e1158973f3b468a864a58980795a502d7adf
Oktay esen <oktayesenn@gmail.com>
2024-02-06 21:00:08 -0500

    Threejs starts has been applied

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,8 @@
 import { motion } from "framer-motion";
-
+import { Suspense } from 'react';
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
+import ceaser from "../assets/ceaser.avif";
 
 const Hero = () => {
   return (
@@ -19,9 +20,10 @@ const Hero = () => {
             Hi, I'm <span className='text-white'>Oktay</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am New York based Developer <br className='sm:block hidden' />
+            New York based Software Developer <br className='sm:block hidden' />
             
           </p>
+
         </div>
       </div>
 
@@ -29,19 +31,6 @@ const Hero = () => {
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
-          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className='w-3 h-3 rounded-full bg-secondary mb-1'
-            />
-          </div>
         </a>
       </div>
     </section>
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Stars } from '@react-three/drei';
+import { useLoader } from '@react-three/fiber';
+import texture from '../../assets/ceaser.avif';
 
-const Computers = () => {
+
+function Computers() {
   return (
-    <div>Computers</div>
-  )
+    <Canvas>
+      <OrbitControls />
+      <Stars />
+      <ambientLight intensity={0.5}/>
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[10, 10, 10]} />
+      <mesh position={[0, 0, 0]}>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color='white' />
+      </mesh>
+    </Canvas>
+  );
 }
 
-export default Computers
+
+
+export default Computers;
